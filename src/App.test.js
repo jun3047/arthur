1990d7793190594915ce51401c5f1b85b137e019
jeong jun <jungjun3047@naver.com>
2023-12-21 15:13:23 +0900

    electron 배포

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';  // 라우터 추가
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
