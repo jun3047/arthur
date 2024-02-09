f28175c7b19a56d4b07f009aba20f49e27ba9264
jeong jun <jungjun3047@naver.com>
2024-02-09 23:21:49 +0900

    [fix] store bookmark 전부 직접 업데이트

--- src/store/store.js
+++ src/store/store.js
@@ -7,15 +7,17 @@ const useStore = create((set, get) => ({
         version: 0 // 초기 버전 번호 추가
     }],
     getBookmark: () => get().bookmark,
-    setBookmark: (bookmark) => set(() => ({ bookmark })),
+    setBookmark: (bookmark) => set(() => {
+      
+      window.electronAPI.setStore('bookmark', bookmark);
+      return bookmark
+    }),
     addBookmark: (title, indexs) => set((state) => {
 
-      const newBookmark = {bookmark: [...state.bookmark, { title, indexs, version: 0 }]}
-      window.electronAPI.setStore('bookmark', newBookmark);
-
-      return ({
-        newBookmark
-        })}
+        const newBookmark = {bookmark: [...state.bookmark, { title, indexs, version: 0 }]}
+        window.electronAPI.setStore('bookmark', newBookmark);
+        return newBookmark
+      }
     ),
     deleteBookmarkByIndex: (title, index) => set((state) => {
 
@@ -29,16 +31,19 @@ const useStore = create((set, get) => ({
 
       window.electronAPI.setStore('bookmark', newBookmark);
 
-      return ({
-        newBookmark
-      })
+      return newBookmark
     }),    
-    deleteBookmarkByTitle: (title) => set((state) => ({
-        bookmark: state.bookmark.filter((bookmark) => bookmark.title !== title).map((mark) => ({
-            ...mark,
-            version: mark.version + 1 // 삭제 동작에 대한 버전 업데이트는 선택적입니다. 객체 자체를 제거하는 경우 필요 없을 수 있습니다.
-        }))
-    })),
+    deleteBookmarkByTitle: (title) => set((state) => {
+
+      const newBookmark = {bookmark: state.bookmark.filter((bookmark) => bookmark.title !== title).map((mark) => ({
+        ...mark,
+        version: mark.version + 1 // 삭제 동작에 대한 버전 업데이트는 선택적입니다. 객체 자체를 제거하는 경우 필요 없을 수 있습니다.
+      }))}
+
+      window.electronAPI.setStore('bookmark', newBookmark);
+      
+      return newBookmark
+  }),
 }));
 
 useStore.subscribe((state) => {
