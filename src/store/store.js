5aff3f85d059cc6575de810fe93ad42c1c6656b4
jeong jun <jungjun3047@naver.com>
2024-02-09 18:21:58 +0900

    [test] bookmark 깊은 검색 활용

--- src/store/store.js
+++ src/store/store.js
@@ -3,22 +3,33 @@ import {create} from 'zustand';
 const useStore = create((set, get) => ({
     bookmark: [{
         title: '모든 북마크',
-        indexs: []
+        indexs: [],
+        version: 0 // 초기 버전 번호 추가
     }],
     getBookmark: () => get().bookmark,
-    setBookmark: (bookmark) => set(() => ({ bookmark: bookmark })),
-    addBookmark: (title, indexs) => set((state) => ({ bookmark: [...state.bookmark, { title: title, indexs: indexs }] })),
+    setBookmark: (bookmark) => set(() => ({ bookmark })),
+    addBookmark: (title, indexs) => set((state) => ({
+        bookmark: [...state.bookmark, { title, indexs, version: 0 }] // 새 북마크 추가 시 초기 버전 설정
+    })),
     deleteBookmarkByIndex: (title, index) => set((state) => ({
-        bookmark: state.bookmark.map((mark) => mark.title === title ? {
-            ...mark,
-            indexs: mark.indexs.filter((idx) => idx !== index)
-        } : mark)
+        bookmark: state.bookmark.map((mark) =>
+            mark.title === title ? {
+                ...mark,
+                indexs: mark.indexs.filter((idx) => idx !== index),
+                version: mark.version + 1 // 인덱스 변경 시 버전 증가
+            } : mark
+        )
     })),    
-    deleteBookmarkByTitle: (title) => set((state) => ({ bookmark: state.bookmark.filter((bookmark) => bookmark.title !== title) })),
+    deleteBookmarkByTitle: (title) => set((state) => ({
+        bookmark: state.bookmark.filter((bookmark) => bookmark.title !== title).map((mark) => ({
+            ...mark,
+            version: mark.version + 1 // 삭제 동작에 대한 버전 업데이트는 선택적입니다. 객체 자체를 제거하는 경우 필요 없을 수 있습니다.
+        }))
+    })),
 }));
 
 useStore.subscribe((state) => {
     window.electronAPI.setStore('bookmark', state.bookmark);
-  }, state => state.bookmark); // bookmark 상태에 대한 변경만 감지  
+  }, state => state.bookmark); // bookmark 상태에 대한 변경만 감지
 
 export default useStore;
