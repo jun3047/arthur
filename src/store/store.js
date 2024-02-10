bb3e16959a0249fd172ae01bd52347186ba86cc9
jeong jun <jungjun3047@naver.com>
2024-02-10 20:22:01 +0900

    [fix] bookmark init 관리 에러 수정

--- src/store/store.js
+++ src/store/store.js
@@ -1,46 +1,71 @@
-import { create } from 'zustand';
+import {create} from 'zustand';
 
-// Zustand 스토어 정의
-const useStore = create((set) => ({
-  bookmark: [], // 초기 상태를 빈 배열로 설정
-  setBookmark: (bookmark) => set({ bookmark }),
-  addBookmark: (title, indexs) => set((state) => {
-    const newBookmark = [...state.bookmark, { title, indexs, version: 0 }];
-    return { bookmark: newBookmark };
-  }),
-  deleteBookmarkByIndex: (title, index) => set((state) => {
-    const newBookmark = state.bookmark.map((mark) =>
-      mark.title === title ? {
+const useStore = create((set, get) => ({
+    bookmark: [
+        {
+          title: '모든 북마크',
+          indexs: [],
+          version: 0 // 초기 버전 번호 추가
+        }
+    ],
+    getBookmark: () => get().bookmark,
+    setBookmark: (bookmark) => set(() => {
+      
+      window.electronAPI.setStore('bookmark', bookmark);
+      return bookmark
+    }),
+    addBookmark: (title, indexs) => set((state) => {
+
+        const newBookmark = {bookmark: [...state.bookmark, { title, indexs, version: 0 }]}
+        window.electronAPI.setStore('bookmark', newBookmark);
+        return newBookmark
+      }
+    ),
+    deleteBookmarkByIndex: (title, index) => set((state) => {
+
+      const newBookmark = {bookmark: state.bookmark.map((mark) =>
+              mark.title === title ? {
+                  ...mark,
+                  indexs: mark.indexs.filter((idx) => idx !== index),
+                  version: mark.version + 1 // 인덱스 변경 시 버전 증가
+              } : mark
+          )}
+
+      window.electronAPI.setStore('bookmark', newBookmark);
+
+      return newBookmark
+    }),    
+    deleteBookmarkByTitle: (title) => set((state) => {
+
+      const newBookmark = {bookmark: state.bookmark.filter((bookmark) => bookmark.title !== title).map((mark) => ({
         ...mark,
-        indexs: mark.indexs.filter((idx) => idx !== index),
-        version: mark.version + 1
-      } : mark
-    );
-    return { bookmark: newBookmark };
-  }),
-  deleteBookmarkByTitle: (title) => set((state) => {
-    const newBookmark = state.bookmark.filter((bookmark) => bookmark.title !== title);
-    return { bookmark: newBookmark };
+        version: mark.version + 1 // 삭제 동작에 대한 버전 업데이트는 선택적입니다. 객체 자체를 제거하는 경우 필요 없을 수 있습니다.
+      }))}
+
+      window.electronAPI.setStore('bookmark', newBookmark);
+      
+      return newBookmark
   }),
 }));
+
 
 // 애플리케이션의 최상위 컴포넌트에서 사용할 초기화 함수
 async function initializeStore() {
   const loadedBookmark = await window.electronAPI.getStore('bookmark');
   if (loadedBookmark) {
+    window.electronAPI.logStore('저장된 게 있어서 가져옴');
+    window.electronAPI.logStore(loadedBookmark);
     useStore.getState().setBookmark(loadedBookmark);
   } else {
     // Electron 저장소에 기본 북마크 설정 (선택적)
     const defaultBookmark = [{ title: '모든 북마크', indexs: [], version: 0 }];
+    
+    window.electronAPI.logStore('저장된 거 없어서 새로 만듬');
+    window.electronAPI.logStore(defaultBookmark);
     useStore.getState().setBookmark(defaultBookmark);
     window.electronAPI.setStore('bookmark', defaultBookmark);
   }
 }
 
-// 상태 변경 시 Electron 저장소에 자동으로 동기화하기 위한 구독 로직
-useStore.subscribe((bookmark) => {
-  window.electronAPI.setStore('bookmark', bookmark);
-}, (state) => state.bookmark);
-
-export default useStore
+export default useStore;
 export { initializeStore };
