3f38fc8ae2b00bdea949d267f1b08eb722fb658e
jeong jun <jungjun3047@naver.com>
2024-02-13 18:40:27 +0900

    [fix] png 이미지 삭제 2

--- txt2json.js
+++ txt2json.js
@@ -2,6 +2,84 @@ const fs = require('fs');
 
 // 여기서에서 fittering을 해야함  -> 1. 제외하는 사진 제거 2. 없애는 사진 제거 3. 그림체에 따른 태그 추가
 
+
+const 뺄거 = [ '9', '10', '24', '25', '29', '46', '56', '63', '86', '90', '117', '171', '176', '177', '186', '192', '203', '292', '363']
+const 필터링 = {
+  '125': '9 12',
+  '133': '13 14 15 16',
+  '141': '5 6 7 8',
+  '148': '6',
+  '151': '5 7 8',
+  '152': '7 15 16',
+  '155': '9 10 14 15',
+  '162': '6 7 8 11 12',
+  '163': '5 6 7 8 16',
+  '168': '6 7 8',
+  '196': '2 5 7 8',
+  '200': '5 6 7 8 13 14',
+  '204': '5 6 7 8 10 11 12',
+  '221': '6 9 10 12',
+  '223': '4 9 10 11 12',
+  '226': '6 7',
+  '227': '9 12',
+  '228': '5 6 7 8 15 16',
+  '235': '6 7 8 14',
+  '240': '8',
+  '214': '16',
+  '215': '5 6 8',
+  '241': '5 10 13 14 15 16',
+  '243': '3 5 6 7 8 13 14 15',
+  '248': '14',
+  '249': '16',
+  '254': '1 3 16',
+  '267': '1 4',
+  '268': '7',
+  '272': '9',
+  '274': '5 6 ',
+  '275': '7 12 5',
+  '279': '7 12 ',
+  '282': '6 7 8 9 13',
+  '285': '6 7 ',
+  '293': '2 5 6 7 12',
+  '294': '7 8 9 10 12 16',
+  '296': '5 6 7',
+  '299': '8 9',
+  '300': '2 3 5 6 7 8',
+  '301': '3',
+  '307': '1 2 16',
+  '309': '4 7 8 11 12',
+  '310': '11',
+  '312': '1 2 4 5 6 7 8 13 14 15 16',
+  '313': '1 3 5 6 7 9 12',
+  '315': '5 6 7',
+  '318': '1 4 9 12',
+  '326': '5',
+  '327': '8',
+  '328': '1 3 9',
+  '329': '9 12',
+  '332': '5 6 7 8',
+  '333': '14 15',
+  '335': '2 6',
+  '343': '13 14 15 16',
+  '353': '10',
+  '358': '12',
+  '360': '5',
+  '361': '12',
+  '366': '3 4 5 7 8 10 11 12 16',
+  '371': '2 5 6 7 8 11 12 13 14 15 16',
+  '378': '10 11',
+  '379': '6 7',
+  '380': '1 4',
+  '384': '10 11 15',
+  '389': '9 10 11 ',
+  '391': '8 10 11 12',
+  '396': '2 5',
+}
+
+const 순서바꾸기 = [
+  '198', '222', '223', '246'
+]
+
 const fetchTxtFile = async () => {
     try {
 
@@ -17,6 +95,14 @@ const fetchTxtFile = async () => {
       let nowLine = 0
 
       while (nowLine < lines.length) {
+
+
+        if (뺄거.includes(i.toString())) {
+          i += 1;
+          nowLine += 4;
+          continue;
+        }
+
         const line1 = lines[nowLine].trim();
         const line2 = lines[nowLine+1].trim();
         const line3 = lines[nowLine+2].trim();
@@ -36,26 +122,53 @@ const fetchTxtFile = async () => {
             },
             tags: tags,
             isBookmark: false,
-          }
+        }
+        // 2 애니 3실사 4 게임
+        const aniData = {
+          ...deflautData,
+          tags: [...tags, '애니메이션'],
+        }
 
-          
+        const realData = {
+          ...deflautData,
+          tags: [...tags, '실사'],
+        }
+
+        const gameData = {
+          ...deflautData,
+          tags: [...tags, '게임'],
+        }
+
+
+ 
         result.push({ index: i.toString() + '.01', ...deflautData});
         result.push({ index: i.toString() + '.02', ...deflautData});
         result.push({ index: i.toString() + '.03', ...deflautData});
         result.push({ index: i.toString() + '.04', ...deflautData});
-        result.push({ index: i.toString() + '.05', ...deflautData});
-        result.push({ index: i.toString() + '.06', ...deflautData});
-        result.push({ index: i.toString() + '.07', ...deflautData});
-        result.push({ index: i.toString() + '.08', ...deflautData});
+        
+        result.push({ index: i.toString() + '.05', ...aniData});
+        result.push({ index: i.toString() + '.06', ...aniData});
+        result.push({ index: i.toString() + '.07', ...aniData});
+        result.push({ index: i.toString() + '.08', ...aniData});
 
-        result.push({ index: i.toString() + '.09', ...deflautData});
-        result.push({ index: i.toString() + '.10', ...deflautData});
-        result.push({ index: i.toString() + '.11', ...deflautData});
-        result.push({ index: i.toString() + '.12', ...deflautData});
-        result.push({ index: i.toString() + '.13', ...deflautData});
-        result.push({ index: i.toString() + '.14', ...deflautData});
-        result.push({ index: i.toString() + '.15', ...deflautData});
-        result.push({ index: i.toString() + '.16', ...deflautData});
+        result.push({ index: i.toString() + '.09', ...realData});
+        result.push({ index: i.toString() + '.10', ...realData});
+        result.push({ index: i.toString() + '.11', ...realData});
+        result.push({ index: i.toString() + '.12', ...realData});
+
+        result.push({ index: i.toString() + '.13', ...gameData});
+        result.push({ index: i.toString() + '.14', ...gameData});
+        result.push({ index: i.toString() + '.15', ...gameData});
+        result.push({ index: i.toString() + '.16', ...gameData});
+
+        const filter = 필터링[i.toString()] ? 필터링[i.toString()].split(' ').map(num => num.trim()) : null;
+
+        if (filter) {
+          filter.forEach(num => {
+            num = num.length === 1 ? '0' + num : num;
+            result.splice(result.findIndex(data => data.index === i.toString() + '.' + num), 1);
+          })
+        }
 
         i += 1;
         nowLine += 4;
